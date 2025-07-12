7892447f31c0179b0f433a7715017878fa6862a2
downloadturbotax2025 <jjiku867@gmail.com>
2025-07-12 11:42:19 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'amazon tv '
 
 
 # The full version, including alpha/beta/rc tags
